ad0f3e6c5d457dd697edada95534db8ad3034147
shahmeer <theshahmeerfsd@gmail.com>
2025-03-16 23:39:02 +0500

    update quantity controls in shopping cart and enhance styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   const increaseQuantity = (id) => {
     setCart(
       cart.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity } : item
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
       )
     );
   };
@@ -22,7 +22,7 @@ function App() {
     setCart(
       cart.map((item) =>
         item.id === id && item.quantity > 1
-          ? { ...item, quantity: item.quantity }
+          ? { ...item, quantity: item.quantity - 1 }
           : item
       )
     );
